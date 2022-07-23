ab3d1a2e3a1b9af3013405bcda3f8733453fbd40
coolasfk <fk.przybyla@gmail.com>
2022-07-23 14:33:47 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,97 +1,52 @@
-
 const computer = document.getElementById('compText');
 const result = document.getElementById('result');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const rock = document.getElementById('rock');
 
-const list = ['paper', 'rock', 'scissors']
+let myChoice = "paper";
 
-paper.addEventListener('click', () => {
-    rock.removeAttribute("style");
-    scissors.removeAttribute("style");
-    paper.style.border="dotted #774c77 1px";
-    playGame1()
+    switch(myChoice) {
+        
+          case 'paper':
+          paper.addEventListener('click', () => {
+              myChoice = "paper"
+              playGame()
+              console.log(myChoice)
+          })
+          case 'rock':
+          rock.addEventListener('click', () => {
+              myChoice = "rock"
+              playGame()
+              console.log(myChoice)
+          })
+          case 'scissors':
+          scissors.addEventListener('click', () => {
+              myChoice = "scissors"
+              playGame()
+              console.log(myChoice)
+          })
+        
 
-})
+const list = ['paper', 'rock', 'scissors']  
 
+function playGame() {
 
-scissors.addEventListener('click', () => {
-    rock.removeAttribute("style");
-    paper.removeAttribute("style");
-   scissors.style.border="dotted #774c77 1px";
-    playGame2()
-
-})
-
-rock.addEventListener('click', () => {
-    paper.removeAttribute("style");
-    scissors.removeAttribute("style");
-    rock.style.border="dotted #774c77 1px";
-    playGame3()
-
-})
-
-
-function playGame1 () {
-    myChoice = 'paper'
-
-   
-        const compChoice = list[Math.floor(Math.random() * list.length)];
-        computer.innerText = compChoice;
-    
-        if(myChoice == compChoice) {
-            result.innerText = `It's a draw, try again!`
-            result.style.color = "white";
-        } else if (myChoice == "scissors" && compChoice == "paper" || myChoice == "paper" && compChoice == "rock" || myChoice == "rock" && compChoice == "scissors" ){
-            result.innerText = 'Congrats! You WON!'
-            result.style.color = "white";
+    const compChoice = list[Math.floor(Math.random() * list.length)];
+    computer.innerText = compChoice;
+        
+     if(myChoice == compChoice) {
+        result.innerText = `It's a draw, try again!`
+        result.style.color = "white";
+         } else if (myChoice == "scissors" && compChoice == "paper" || myChoice == "paper" && compChoice == "rock" || myChoice == "rock" && compChoice == "scissors" ){
+        result.innerText = 'Congrats! You WON!'
+         result.style.color = "white";
         } else {
-            result.innerText = 'Computer beat you up, you loser!'
-            result.style.color = "white";
-        }
+        result.innerText = 'Computer beat you up, you loser!'
+        result.style.color = "white";
+         }
+     }
     }
-
-    function playGame2 () {
-        myChoice = 'scissors'
-    
-       
-            const compChoice = list[Math.floor(Math.random() * list.length)];
-            computer.innerText = compChoice;
-        
-            if(myChoice == compChoice) {
-                result.innerText = `It's a draw, try again!`
-                result.style.color = "white";
-            } else if (myChoice == "scissors" && compChoice == "paper" || myChoice == "paper" && compChoice == "rock" || myChoice == "rock" && compChoice == "scissors" ){
-                result.innerText = 'Congrats! You WON!'
-                result.style.color = "white";
-            } else {
-                result.innerText = 'Computer beat you up, you loser!'
-                result.style.color = "white";
-            }
-        }
-
-        function playGame3 () {
-            myChoice = 'rock'
-        
-           
-                const compChoice = list[Math.floor(Math.random() * list.length)];
-                computer.innerText = compChoice;
-            
-                if(myChoice == compChoice) {
-                    result.innerText = `It's a draw, try again!`
-                    result.style.color = "white";
-                } else if (myChoice == "scissors" && compChoice == "paper" || myChoice == "paper" && compChoice == "rock" || myChoice == "rock" && compChoice == "scissors" ){
-                    result.innerText = 'Congrats! You WON!'
-                    result.style.color = "white";
-                } else {
-                    result.innerText = 'Computer beat you up, you loser!'
-                    result.style.color = "white";
-                }
-            }
-
-
-
 
 
 
